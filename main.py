c0e4c2a66b378d4bbc04d34c9e5099e29a3477d2
kailing.peng@ost.ch <kailing.peng@ost.ch>
2021-12-23 04:18:12 +0100

    add subparses

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import csv
 import os
+import sys
 from collections import Counter
 from pathlib import Path
 import requests
 import argparse
 from random import choice
 
-all_names = []
-all_ages = []
 genders = ['m', 'w']
 
 available_data = {
-    '2021': 20210103,
-    '2020': 20200306,
-    '2019': 20190304,
-    '2018': 20180305,
-    '2017': 20170308,
-    '2016': 20160307,
-    '2015': 20151001,
+    2021: 20210103,
+    2020: 20200306,
+    2019: 20190304,
+    2018: 20180305,
+    2017: 20170308,
+    2016: 20160307,
+    2015: 20151001,
 }
 
 
@@ -27,44 +26,54 @@ def find(dog_name, data_year):
     if not path.ok:
         print(path.status_code, path.reason)
         return None
-
+    results = {}
     content = path.content.decode("utf-8-sig").splitlines()
     reader = csv.DictReader(content)
     for line in reader:
         if dog_name == line['HUNDENAME']:
-            print(line["GEBURTSJAHR_HUND"], line["GESCHLECHT_HUND"])
+            results[line["GEBURTSJAHR_HUND"]] = line["GESCHLECHT_HUND"]
+    return results
 
 
 def get_stats(data_year):
+    all_names = []
+    all_ages = []
+    all_genders = []
+    top_names = ''
     path = requests.get(
         f'https://data.stadt-zuerich.ch/dataset/sid_stapo_hundenamen/download/{available_data[data_year]}_hundenamen.csv')
+
     if not path.ok:
         print(path.status_code, path.reason)
         return None
 
     content = path.content.decode("utf-8-sig").splitlines()
     reader = csv.DictReader(content)
-    all_genders = []
+
+    if data_year == 2018:
+        for value in reader:
+            all_genders.append(value['geschlecht_hund'])
+            all_names.append(value['hundename'])
+            all_ages.append(value['geburtsjahr_hund'])
 
     for value in reader:
         all_genders.append(value['GESCHLECHT_HUND'])
         all_names.append(value['HUNDENAME'])
         all_ages.append(value['GEBURTSJAHR_HUND'])
 
-    male, female = Counter(genders).values()
+    male, female = Counter(all_genders).values()
     top_10_names = {k: v for k, v in sorted(Counter(all_names).items(), key=lambda item: item[1], reverse=True)[:10]}
-    top_names = ''
+
     for i in top_10_names.keys():
         top_names = top_names + " " + i
-
-    print("Shortest Name: {}\nLongest Name: {}\nTotal male dogs: {}\nTotal Female dogs: {}\nTop 10 Names: {}".format(
-        min(all_names, key=len),
-        max(all_names, key=len),
-        male,
-        female, top_names))
+    print(
+        "longes:{}, shortest:{},male:{}, female:{}, popular:{}".format(max(all_names, key=len), min(all_names, key=len),
+                                                                       male, female, top_names))
+    return all_names, all_ages
 
 
 def generate_dog(target_directory):
+    global all_names, all_ages
     dog_name = choice(all_names)
     dog_birth = choice(all_ages)
     dog_gender = choice(['m', 'w'])
@@ -101,34 +110,48 @@ def generate_dog(target_directory):
         "".format(dog_name, dog_birth, dog_gender, dog_image))
 
 
-def run(dog_name, year, stats, create, target_directory):
-    try:
-        if dog_name:
-            find(dog_name, year)
-        elif stats:
-            get_stats(year)
-        elif create:
-            generate_dog(target_directory)
-    except KeyError:
-        print('Invalid input!')
-
-
 def get_parser():
     parser = argparse.ArgumentParser(description="Your input options: ")
-    parser.add_argument('-n', '--name', help='Search a dog by name', type=str)
-    parser.add_argument('-s', '--statistics', help='Read the statistics report from the year')
-    parser.add_argument('-y', '--year', help='Specify the data year', default='2021')
-    parser.add_argument('-c', '--create', help='Create a random dog')
-    parser.add_argument('-o', '--output-dir',
-                        help='Specify a location to save the image of a new dog, default is current directory',
-                        type=str, default='.')
+    sub_parser = parser.add_subparsers()
+
+    find_dog = sub_parser.add_parser("find")
+    find_dog.add_argument('name', type=str, help='the dog name you want to search')
+    find_dog.add_argument('-y', '--year', type=int, help='Specify the data year', default=2021)
+
+    read_stats = sub_parser.add_parser("stats")
+    read_stats.add_argument('-y', '--year', type=int, help='Specify the data year', default=2021)
+
+    create_dog = sub_parser.add_parser("create")
+    create_dog.add_argument('-o', '--output-dir',
+                            help='Specify a location to save the image of a new dog, default is current directory',
+                            type=str, default='.')
     return parser.parse_args()
 
 
 def main(args=None):
-    parser = get_parser()
-    run(dog_name=parser.name, year=parser.year, stats=parser.statistics, create=parser.create,
-        target_directory=parser.output_dir)
+    args = get_parser()
+
+    try:
+        if sys.argv[1] == "find":
+            search_result = find(dog_name=args.name, data_year=args.year).items()
+            # TODO: PRINT RESULT NOT FOUND WHEN NO MATCHING
+            if not search_result:
+                print("NOT FOUND")
+            else:
+                for year, gender in search_result:
+                    print(year, gender)
+
+    except KeyError:
+        print("Available date from 2015-2021")
+
+    if sys.argv[1] == "stats":
+        get_stats(data_year=args.year)
+
+
+    if sys.argv[1] == "create":
+        names, years = get_stats(data_year=args.year)
+
+        generate_dog(target_directory=args.output_dir, name = names)
 
 
 if __name__ == "__main__":
